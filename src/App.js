e42dddcc7e460009f11ae013213a78630ef9e5da
MenghySanchez <47823011+MenghySanchez@users.noreply.github.com>
2022-03-01 19:25:08 -0500

    primer commit

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>FEUPE</h1>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://feupe.github.io"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Federación de Estudiantes Universitarios Particulares del Ecuador
         </a>
       </header>
     </div>
